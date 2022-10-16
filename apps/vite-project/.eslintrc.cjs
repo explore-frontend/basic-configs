31e0976c0247f9b0b0ddf15ad4210e49dcd6c554
Amorites <751809522@qq.com>
2022-10-16 13:00:00 +0800

    feat: add test project

---
+++ apps/vite-project/.eslintrc.cjs
@@ -0,0 +1,14 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+module.exports = {
+    root: true,
+    extends: ['@kwai-explore/eslint-config/preset/vue3-ts'],
+    overrides: [
+        {
+            files: ['**/*.ts', '**/*.vue'],
+            parserOptions: {
+                project: ['./tsconfig.json'],
+                tsconfigRootDir: __dirname,
+            },
+        },
+    ],
+};
