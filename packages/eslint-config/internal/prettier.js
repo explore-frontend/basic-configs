231db8adef6f7eac400f9ce34195e664fc4e65fa
ZHAO JinXiang <xiaoxiangmoe@gmail.com>
2023-06-30 20:16:25 +0800

    chore: format-fix

--- packages/eslint-config/internal/prettier.js
+++ packages/eslint-config/internal/prettier.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     rules: {
         /**
          * See https://github.com/prettier/eslint-config-prettier/blob/main/README.md#curly
-         * 
+         *
          */
         curly: ['error', 'all'],
     },
