e69310a462258f4747044c4a36877deb8ccf1579
lsdsjy <lsdsjy@163.com>
2023-11-17 14:10:54 +0800

    feat: not prefer `??` for string and number

---
+++ packages/eslint-config-testing/samples/ts/src/@typescript-eslint/prefer-nullish-coalescing/nullish.ts
@@ -0,0 +1,9 @@
+declare const a: {} | null
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing -- 对象不允许用 ||
+const na = a || {}
+
+// 字符串和数字可以用 ||
+declare const s: string | null
+const ns = s || ''
+declare const n: number | null
+const nn = n || ''
